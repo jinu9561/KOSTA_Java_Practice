b941aacafd75f88274a550fb9f97861272d27f83
DESKTOP-BSOJMUG\jinwoojung <jinu9561@gmail.com>
2024-02-10 19:01:46 +0900

    Revert "step4"
    This reverts commit b5910a4969bdee73cb20d0d0fb789d375cfd83c0.

--- step05_Collection/src/ex0206/homework/StartView.java
+++
@@ -1,9 +0,0 @@
-package ex0206.homework;
-class StartView {
-	public static void main(String[] args) {
-		System.out.println("-----------프로그램 시작합니다. -----------------");
-        
-	    new MenuView().printMenu();
-	}
-}
-
